d644deb2a846d52146c60b5ba859b61c8198ad5d
donioscode <doniyornormuxammedov@gmail.com>
2024-04-25 11:20:12 +0500

    all componnents added

---
+++ BMI-Calculator-MVC-Demo/Controller/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  BMI-Calculator-MVC-Demo
+//
+//  Created by doniyor normuxammedov on 25/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
