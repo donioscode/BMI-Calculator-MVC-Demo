869ec6fd975781cb5a89266b552b794a3a8338d6
donioscode <doniyornormuxammedov@gmail.com>
2024-04-25 10:30:44 +0500

    Initial Commit

---
+++ BMI-Calculator-MVC-Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI-Calculator-MVC-Demo
+//
+//  Created by doniyor normuxammedov on 25/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
