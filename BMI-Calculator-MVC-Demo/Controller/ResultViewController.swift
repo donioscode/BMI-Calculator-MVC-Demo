867bbc7094cb49ea6e5f8e2880f8938a057dca5e
donioscode <doniyornormuxammedov@gmail.com>
2024-04-26 15:53:30 +0500

    connected labels

--- BMI-Calculator-MVC-Demo/Controller/ResultViewController.swift
+++ BMI-Calculator-MVC-Demo/Controller/ResultViewController.swift
@@ -9,21 +9,17 @@ import UIKit
 
 class ResultViewController: UIViewController {
 
+    @IBOutlet weak var resultLB: UILabel!
+    @IBOutlet weak var adviceLB: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func recalculateBtnPressed(_ sender: UIButton) {
+        
     }
-    */
-
 }
